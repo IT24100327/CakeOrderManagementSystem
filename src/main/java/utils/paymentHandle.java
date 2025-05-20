abdee430839d37b8ce23fa0e262c0517ca77b678
IT24100327 <it24100327@my.sliit.lk>
2025-05-20 13:53:49 +0530

    added some methods to payment handle

--- src/main/java/utils/paymentHandle.java
+++ src/main/java/utils/paymentHandle.java
@@ -5,10 +5,18 @@ import entities.Payment;
 import java.io.*;
 import java.util.LinkedList;
 
-public class paymentHandle {
+public class PaymentHandle {
     private static final LinkedList<Payment> payments = new LinkedList<>();
     private static final String FILE_PATH = "E:/Data/payments.txt";
     private static int lastPaymentId = 0;
+
+    static {
+        try {
+            loadFromFile();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     private static String generatePaymentId() {
         lastPaymentId++;
@@ -59,6 +67,27 @@ public class paymentHandle {
         }
         return null;
     }
+
+    public static void setPaymentStatus(String paymentId, String paymentStatus) throws IOException {
+        for (Payment payment : payments) {
+            if (payment.getPaymentId().equals(paymentId)) {
+                payment.setPaymentStatus(paymentStatus);
+            }
+        }
+        saveToFile();
+    }
+
+    public static void setPaymentDetails(String paymentId, double paymentAmount, String paymentStatus, String paymentMethod) throws IOException {
+        for (Payment payment : payments) {
+            if (payment.getPaymentId().equals(paymentId)) {
+                payment.setPaymentStatus(paymentStatus);
+                payment.setPaymentMethod(paymentMethod);
+                payment.setPaymentAmount(paymentAmount);
+            }
+        }
+        saveToFile();
+    }
+
 
     public static void sortOrderByPaymentDate() {
         BubbleSorter.bubbleSortByDeliveryDate(payments);
